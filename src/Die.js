c792cfaec623e559d11a0d1d8172fc4ae1d92790
Henovad <88333431+Henovad@users.noreply.github.com>
2021-12-18 21:27:11 +0200

    Update Die.js

--- src/Die.js
+++ src/Die.js
@@ -11,7 +11,6 @@ export default function Die(props){
     return (
     <div className="die" style={styles} onClick={props.holdDice}>
         <img className="dice-img" alt={props.value} src={`http://roll.diceapi.com/images/poorly-drawn/d6/${props.value}.png`} />
-        <h1>{props.value} aa</h1>
     </div>
     )
     
